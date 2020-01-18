cf7250ba096a4602e49b62f8a9f2924dc58a1571
Nick Nelson <whoisnicknelson@gmail.com>
2020-01-18 14:13:19 -0800

    adding jekyll files, styles, data

---
+++ assets/js/scripts.js
@@ -0,0 +1,39 @@
+(function($){
+    $.fn.extend({ 
+        rotateText: function(options) {
+            var defaults = {
+                fadeSpeed: 500,
+                pauseSpeed: 100,
+                child:null
+            };
+             
+            var options = $.extend(defaults, options);
+         
+            return this.each(function() {
+                  var o =options;
+                  var obj = $(this);                
+                  var items = $(obj.children(), obj);
+                  items.each(function() {$(this).hide();})
+                  if(!o.child){
+                    var next = $(obj).children(':first');
+                  }else{
+                    var next = o.child;
+                  }
+                  $(next).fadeIn(o.fadeSpeed, function() {
+                        $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function() {
+                            var next = $(this).next();
+                            if (next.length == 0){
+                                    next = $(obj).children(':first');
+                            }
+                            $(obj).rotateText({child : next, fadeSpeed : o.fadeSpeed, pauseSpeed : o.pauseSpeed});
+                        })
+                    });
+            });
+        }
+    });
+})(jQuery);
+
+ $(document).ready(function() {
+    console.log('doc read')
+    $('.rotate').rotateText({fadeSpeed:500, pauseSpeed:1500});
+ });
